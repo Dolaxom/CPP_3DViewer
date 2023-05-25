3e49475aef68426ce07ff1ff6476ae860b54f98a
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2023-05-26 01:00:05 +0300

    [Project][Root] Adding Root-Singleton class

---
+++ src/viewer/s21_root.cpp
@@ -0,0 +1,28 @@
+﻿#include "s21_root.h"
+
+namespace s21 {
+S21Window& S21Root::getWindow() {
+  static S21Window s21Window{windowWidth_, windowHeight_, "3DViewer"};
+  return s21Window;
+}
+
+S21Device& S21Root::getDevice() {
+  static S21Device s21Device{getWindow()};
+  return s21Device;
+}
+
+S21RendererComponent& S21Root::getRendererComponent() {
+  static S21RendererComponent s21Renderer{getWindow(), getDevice()};
+  return s21Renderer;
+}
+
+S21View& S21Root::getView() {
+  static S21View s21View;
+  return s21View;
+}
+
+std::vector<S21Object>& S21Root::getObject() {
+  static std::vector<S21Object> s21Objects;
+  return s21Objects;
+}
+}  // namespace s21
